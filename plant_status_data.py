2848017119fd63b6959501f2da6922bcaa87b33a
김동건 <donggunmaru@gmail.com>
2024-11-29 06:08:56 +0900

    cds 값 범위 수정

--- plant_status_data.py
+++ plant_status_data.py
@@ -42,10 +42,10 @@ class Sensor(threading.Thread):
             self.lastMillis = curMillis
 
             # map 1023~0 to 0~100
-            data.soil_moisture = (1023 - self.spi_reader.analog_read(SOIL_MOISTURE)) * 100 /1024
+            data.soil_moisture = (1023 - self.spi_reader.analog_read(SOIL_MOISTURE)) * 100 / 1024
             data.temperature = self.dht_reader.read_temperature()
             data.humidity = self.dht_reader.read_humidity()
-            # map 0~600 to 0~100
-            data.illuminance = self.spi_reader.analog_read(ILLUMINANCE) / 6
+            # map 0~1023 to 0~100
+            data.illuminance = self.spi_reader.analog_read(ILLUMINANCE) * 100 / 1024
 
             # print("sm: {}\ttemp: {}\thum: {}\till: {}".format(data.soil_moisture, data.temperature, data.humidity, data.illuminance))
